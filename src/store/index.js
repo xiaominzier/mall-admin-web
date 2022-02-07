ad7ceca85600df2b558c7d9e23ed7b12802e7d95
xiaominzier <17610369209@163.com>
2022-02-07 16:42:12 +0800

    .

--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,18 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    // 用于切换菜单的闭合状态  false代表不闭合  true代表闭合(也可以通过父组件传子组件的方式，但是太麻烦，每个组件都要添加)
+    collapsed: false,
   },
   mutations: {
+    changeCollapsed(state) {
+      state.collapsed = !state.collapsed;
+    },
   },
   actions: {
+    changeCollapsed({ commit }) {
+      commit('changeCollapsed');
+    },
   },
   modules: {
   },
